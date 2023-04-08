cd5f3e5129263a0de3440f5ac9b7744e92d36135
Asher <asher_github@163.com>
2023-04-08 15:52:23 +0800

    added cve-3000

--- utils/commom.py
+++ utils/commom.py
@@ -9,9 +9,9 @@ from pathlib import Path
 
 TASK_NAME = 'SV'
 DATA_DIR = './data/datasets/'
-TRAIN_FILE = DATA_DIR + TASK_NAME + '/cve-2000.jsonl'
-EVAL_FILE = DATA_DIR + TASK_NAME + '/cve-2000.jsonl'
-PREDICT_FILE = DATA_DIR + TASK_NAME + '/cve-2000.jsonl'
+TRAIN_FILE = DATA_DIR + TASK_NAME + '/cve-3000.jsonl'
+EVAL_FILE = DATA_DIR + TASK_NAME + '/cve-3000.jsonl'
+PREDICT_FILE = DATA_DIR + TASK_NAME + '/cve-3000.jsonl'
 
 LABELS_FILE = DATA_DIR + 'labels.txt'
 
