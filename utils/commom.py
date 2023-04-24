44b97e8492c1f5f4771ab776a4a5837f6baceaab
Asher <asher_github@163.com>
2023-04-24 18:18:50 +0800

    increased the size of dataset to 4k,and switch to use 4k dataset to training.

--- utils/commom.py
+++ utils/commom.py
@@ -9,9 +9,9 @@ from pathlib import Path
 
 TASK_NAME = 'SV'
 DATA_DIR = './data/datasets/'
-TRAIN_FILE = DATA_DIR + TASK_NAME + '/cve-3600.jsonl'
-EVAL_FILE = DATA_DIR + TASK_NAME + '/cve-3600.jsonl'
-PREDICT_FILE = DATA_DIR + TASK_NAME + '/cve-3600.jsonl'
+TRAIN_FILE = DATA_DIR + TASK_NAME + '/cve-4000.jsonl'
+EVAL_FILE = DATA_DIR + TASK_NAME + '/cve-4000.jsonl'
+PREDICT_FILE = DATA_DIR + TASK_NAME + '/cve-4000.jsonl'
 
 LABELS_FILE = DATA_DIR + 'labels.txt'
 
